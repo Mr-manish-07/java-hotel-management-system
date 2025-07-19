79ae0c674d9708552d1365a932eac16d52cc1f77
Manish Kumar <manish825316@gmail.com>
2025-07-19 21:41:36 +0530

    Generic Doa Implementaion consists of insert,delete,update,findById,findAll

--- src/main/java/org/manish07/dao/impl/GenericsDAOImpl.java
+++ src/main/java/org/manish07/dao/impl/GenericsDAOImpl.java
@@ -1,7 +1,115 @@
 package org.manish07.dao.impl;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.manish07.dao.GenericsDAO;
 
-public class GenericsDAOImpl<T> implements GenericsDAO<T> {
+import java.util.List;
 
+public class GenericsDAOImpl<T> implements GenericsDAO<T> {
+    
+    private final SessionFactory sessionFactory;
+    private final Class<T> entityClass;
+    
+    public GenericsDAOImpl(SessionFactory sessionFactory, Class<T> entityClass) {
+        this.sessionFactory = sessionFactory;
+        this.entityClass = entityClass;
+    }
+    
+    
+    
+    @Override
+    public boolean save (T entity) {
+        
+        boolean result = false;
+        Transaction tx = null;
+        
+        try (Session session = sessionFactory.openSession ()) {
+            
+            tx = session.beginTransaction ();
+            session.persist (entity);
+            tx.commit ();
+            result = true;
+            
+        }
+        catch (Exception e) {
+            
+            if (tx != null) tx.rollback ();
+            e.printStackTrace ();
+        }
+        return result;
+    }
+    
+    
+    
+    @Override
+    public boolean update (T entity) {
+        
+        boolean result = false;
+        Transaction tx = null;
+        
+        try (Session session = sessionFactory.openSession ()) {
+            
+            tx = session.beginTransaction ();
+            session.merge (entity);
+            tx.commit ();
+            result = true;
+            
+        }
+        catch (Exception e) {
+            
+            if (tx != null) tx.rollback ();
+            e.printStackTrace ();
+        }
+        return result;
+    }
+    
+    
+    
+    @Override
+    public boolean delete (T entity) {
+        
+        boolean result = false;
+        Transaction tx = null;
+        
+        try (Session session = sessionFactory.openSession ()) {
+            
+            tx = session.beginTransaction ();
+            
+            if (entity != null) {
+                session.remove (entity);  // I'm using JPA standard for deletion
+                result = true;
+            }
+            tx.commit ();
+            
+        }
+        catch (Exception e) {
+            
+            if (tx != null) tx.rollback ();
+            e.printStackTrace ();
+        }
+        return result;
+    }
+    
+    
+    
+    @Override
+    public T findById (int id) {
+        
+        try (Session session = sessionFactory.openSession ()) {
+            return session.find (entityClass,id);
+        }
+    }
+    
+    
+    
+    @Override
+    public List<T> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM " + entityClass.getSimpleName(), entityClass)
+                    .getResultList();
+        }
+    }
+    
 }
--- src/main/java/org/manish07/util/DBUtil.java
+++ src/main/java/org/manish07/util/DBUtil.java
@@ -5,7 +5,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class DBUtil {
+    
     private static final SessionFactory sessionFactory;
+    
     static {
         try {
             sessionFactory = new Configuration ().configure ().buildSessionFactory ();
@@ -18,5 +20,4 @@ public class DBUtil {
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
-
 }
